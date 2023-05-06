2983089296f6313adce1b6958277505a771df292
NextGen20 <amitbachar2@gmail.com>
2023-05-06 19:03:30 +0300

    d

--- first-app/firstapp.py
+++ first-app/firstapp.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/")
+def hello():
+    return "Hello World!"
 
-if __name__ == '_main_':
-    app.run(host='0.0.0.0', port=5001)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
